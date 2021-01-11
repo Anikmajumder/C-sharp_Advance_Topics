cd48c9284becf39c0262655ae2d0f91a7212b713
Anikmajumder <53091562+Anikmajumder@users.noreply.github.com>
2021-01-11 17:56:09 +0600

    Generics

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;  
+namespace CSharpProgram
+{
+    class GenericClass<T>
+    {
+        public GenericClass(T msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericClass<string> gen = new GenericClass<string>("This is generic class");
+            GenericClass<int> genI = new GenericClass<int>(101);
+            GenericClass<char> getCh = new GenericClass<char>('I');
+        }
+    }
+}
