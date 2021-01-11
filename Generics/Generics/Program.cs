0178ac54f6df36f6b2c7e998ad5a7c2c3db983ab
Anikmajumder <53091562+Anikmajumder@users.noreply.github.com>
2021-01-11 18:01:13 +0600

    Update Program.cs

--- Generics/Generics/Program.cs
+++ Generics/Generics/Program.cs
@@ -7,6 +7,22 @@ namespace CSharpProgram
         {
             Console.WriteLine(msg);
         }
+
+
+       
+
+
+
+    }
+
+
+
+    class AnotherGenericClass
+    {
+        public void Show<T>(T sms)
+        {
+            Console.WriteLine(sms);
+        }
     }
     class Program
     {
@@ -15,6 +31,12 @@ namespace CSharpProgram
             GenericClass<string> gen = new GenericClass<string>("This is generic class");
             GenericClass<int> genI = new GenericClass<int>(101);
             GenericClass<char> getCh = new GenericClass<char>('I');
+
+
+            AnotherGenericClass genC = new AnotherGenericClass();
+            genC.Show("This is generic method");
+            genC.Show(101);
+            genC.Show('I');
         }
     }
 }
